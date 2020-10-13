32b3f5c62a09fb267805e9902ac0eb22004ed2cf
Kent Chadwick <kento.mc@gmail.com>
2020-10-13 23:14:09 +0100

    Implemented hillfort array

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -1,4 +1,4 @@
-package org.wit.hillfort
+package org.wit.hillfort.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,17 +6,28 @@ import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
+import org.wit.hillfort.R
+import org.wit.hillfort.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
+
+    var hillfort = HillfortModel()
+    val hillforts = ArrayList<HillfortModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
         info("Hillfort Activity started..")
 
         btnAdd.setOnClickListener() {
-            val hillfortTitle = hillfortTitle.text.toString()
-            if (hillfortTitle.isNotEmpty()) {
-                info("add Button Pressed: $hillfortTitle")
+            hillfort.title = hillfortTitle.text.toString()
+            hillfort.description = description.text.toString()
+            if (hillfort.title.isNotEmpty()) {
+                hillforts.add(hillfort.copy())
+                info("add Button Pressed: ${hillfort}")
+                for (i in hillforts.indices) {
+                    info("Hillfort[$i]:${this.hillforts[i]}")
+                }
             }
             else {
                 toast ("Please Enter a title")
