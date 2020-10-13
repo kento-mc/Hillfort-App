ebbe76288e34abd49bf92855ddc617f1875849fa
Kent Chadwick <kento.mc@gmail.com>
2020-10-14 00:16:27 +0100

    List of cards displaying

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -7,26 +7,27 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
 import org.wit.hillfort.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     var hillfort = HillfortModel()
-    val hillforts = ArrayList<HillfortModel>()
+    lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
-        info("Hillfort Activity started..")
+        app = application as MainApp
 
         btnAdd.setOnClickListener() {
             hillfort.title = hillfortTitle.text.toString()
             hillfort.description = description.text.toString()
             if (hillfort.title.isNotEmpty()) {
-                hillforts.add(hillfort.copy())
+                app.hillforts.add(hillfort.copy())
                 info("add Button Pressed: ${hillfort}")
-                for (i in hillforts.indices) {
-                    info("Hillfort[$i]:${this.hillforts[i]}")
+                for (i in app.hillforts.indices) {
+                    info("Hillfort[$i]:${app.hillforts[i]}")
                 }
             }
             else {
