0a192302ba2eda05d63e2ff51c7d58b74cc8e602
Kent Chadwick <kento.mc@gmail.com>
2020-10-14 01:01:37 +0100

    Implemented adding of new hillforts to list and cancelling the add activity

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -2,6 +2,8 @@ package org.wit.hillfort.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -18,6 +20,10 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
+        info("Hillfort Activity started..")
+
         app = application as MainApp
 
         btnAdd.setOnClickListener() {
@@ -29,10 +35,26 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
                 for (i in app.hillforts.indices) {
                     info("Hillfort[$i]:${app.hillforts[i]}")
                 }
+                setResult(AppCompatActivity.RESULT_OK)
+                finish()
             }
             else {
                 toast ("Please Enter a title")
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_hillfort, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
