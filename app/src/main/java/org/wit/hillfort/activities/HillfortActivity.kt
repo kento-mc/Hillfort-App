5eae04e343891aedc7686460032046a34c368cae
Kent Chadwick <kento.mc@gmail.com>
2020-10-27 10:41:11 +0000

    Added dummy file for api key template

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -8,6 +8,7 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.toast
 import org.wit.hillfort.R
 import org.wit.hillfort.helpers.readImage
@@ -64,6 +65,10 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     chooseImage.setOnClickListener {
       showImagePicker(this, IMAGE_REQUEST)
+    }
+
+    hillfortLocation.setOnClickListener {
+      startActivity (intentFor<MapActivity>())
     }
   }
 
