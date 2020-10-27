990e67ae22a185d5f653e74aba48c23b6b795453
Kent Chadwick <kento.mc@gmail.com>
2020-10-27 16:52:45 +0000

    Local JSON storage implemented

--- app/src/main/java/org/wit/hillfort/main/MainApp.kt
+++ app/src/main/java/org/wit/hillfort/main/MainApp.kt
@@ -3,6 +3,7 @@ package org.wit.hillfort.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.hillfort.models.HillfortJSONStore
 import org.wit.hillfort.models.HillfortMemStore
 import org.wit.hillfort.models.HillfortStore
 
@@ -12,7 +13,7 @@ class MainApp : Application(), AnkoLogger {
 
     override fun onCreate() {
         super.onCreate()
-        hillforts = HillfortMemStore()
+        hillforts = HillfortJSONStore(applicationContext)
         info("Hillfort started")
     }
 }
