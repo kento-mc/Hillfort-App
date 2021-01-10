98993a89af16a353ad1342cdd8e85af96714598a
Kent Chadwick <kento.mc@gmail.com>
2021-01-10 21:07:22 +0000

    Added swipe functionality to favorites list view

--- app/src/main/java/org/wit/hillfort/views/favoritelist/FavoriteListView.kt
+++ app/src/main/java/org/wit/hillfort/views/favoritelist/FavoriteListView.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
 import kotlinx.android.synthetic.main.card_hillfort.*
@@ -12,6 +15,8 @@ import org.jetbrains.anko.*
 import org.wit.hillfort.R
 import org.wit.hillfort.models.HillfortModel
 import org.wit.hillfort.models.UserModel
+import org.wit.hillfort.utils.SwipeToDeleteCallback
+import org.wit.hillfort.utils.SwipeToEditCallback
 import org.wit.hillfort.views.BaseView
 import org.wit.hillfort.views.hillfortlist.HillfortAdapter
 import org.wit.hillfort.views.hillfortlist.HillfortListPresenter
@@ -34,11 +39,32 @@ class FavoriteListView : BaseView(),
     recyclerView.layoutManager = layoutManager
 
     presenter.getHillforts()
+    setSwipeRefresh()
+
+    val swipeEditHandler = object : SwipeToEditCallback(this) {
+      override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        val hillfortSwiped = presenter.app.hillforts.findByFbId(viewHolder.itemView.tag as String)
+        onHillfortClick(hillfortSwiped!!)
+      }
+    }
+    val itemTouchEditHelper = ItemTouchHelper(swipeEditHandler)
+    itemTouchEditHelper.attachToRecyclerView(recyclerView)
+
+    val swipeDeleteHandler = object : SwipeToDeleteCallback(this) {
+      override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        val adapter = recyclerView.adapter as FavoriteAdapter
+        deleteFromList(viewHolder.itemView.tag as String)
+        adapter.removeAt(viewHolder.adapterPosition)
+      }
+    }
+    val itemTouchDeleteHelper = ItemTouchHelper(swipeDeleteHandler)
+    itemTouchDeleteHelper.attachToRecyclerView(recyclerView)
   }
 
   override fun showHillforts(hillforts: ArrayList<HillfortModel>) {
     recyclerView.adapter = FavoriteAdapter(hillforts, this)
     recyclerView.adapter?.notifyDataSetChanged()
+    checkSwipeRefresh()
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -74,5 +100,22 @@ class FavoriteListView : BaseView(),
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
     presenter.getHillforts()
     super.onActivityResult(requestCode, resultCode, data)
+  }
+
+  fun setSwipeRefresh() {
+    swiperefresh.setOnRefreshListener(object : SwipeRefreshLayout.OnRefreshListener {
+      override fun onRefresh() {
+        swiperefresh.isRefreshing = true
+        presenter.doCheckForUpdates()
+      }
+    })
+  }
+
+  fun checkSwipeRefresh() {
+    if (swiperefresh.isRefreshing) swiperefresh.isRefreshing = false
+  }
+
+  fun deleteFromList(fbId: String) {
+    presenter.doDeleteFromList(fbId)
   }
 }
